bc6fa1b6fbcf4ac1dd4ccfbd017cd92141360364
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2025-09-12 21:35:28 +0100

    Add PAP3 support to FF-777

--- src/B738BankAngleCommands.lua
+++ src/B738BankAngleCommands.lua
@@ -1,4 +1,23 @@
+function AdjustBankAngle(actualPosition, desiredPosition, upCommand, downCommand)
+    local difference = actualPosition - desiredPosition
+    if difference == 0 then
+        return
+    end
+
+    local command = difference < 0
+        and upCommand
+        or downCommand
+
+    local absDifference = math.abs(difference)
+
+    for _ = 1, absDifference do
+        command_once(command)
+    end
+end
+
 if (PLANE_ICAO == "B738") then
+    dataref("actualPosition", "laminar/B738/autopilot/bank_angle_pos", "readonly")
+
     create_command("MobiFlight/B738/BankAngle10",
         "BankAngle10",
         "AdjustBankAngle(0)",
@@ -25,22 +44,46 @@ if (PLANE_ICAO == "B738") then
         "",
         "")
 
-    dataref("actualPosition", "laminar/B738/autopilot/bank_angle_pos", "readonly")
+    function Adjust738BankAngle(desiredPosition)
+        AdjustBankAngle(actualPosition, desiredPosition, 'laminar/B738/autopilot/bank_angle_up', 'laminar/B738/autopilot/bank_angle_dn')
+    end
+end
 
-    function AdjustBankAngle(desiredPosition)
-        local difference = actualPosition - desiredPosition
-        if difference == 0 then
-            return
-        end
+if (PLANE_ICAO == "B772") then
+    dataref("actualPosition", "1-sim/ckpt/mcpBankAngleSwitch/anim", "readonly")
 
-        local command = difference < 0
-            and 'laminar/B738/autopilot/bank_angle_up'
-            or 'laminar/B738/autopilot/bank_angle_dn'
+    create_command("MobiFlight/B777/BankAngleAuto",
+        "BankAngleAuto",
+        "AdjustBankAngle(0)",
+        "",
+        "")
+    create_command("MobiFlight/B777/BankAngle5",
+        "BankAngle05",
+        "AdjustBankAngle(1)",
+        "",
+        "")
+    create_command("MobiFlight/B777/BankAngle10",
+        "BankAngle10",
+        "AdjustBankAngle(2)",
+        "",
+        "")
+    create_command("MobiFlight/B777/BankAngle15",
+        "BankAngle15",
+        "AdjustBankAngle(3)",
+        "",
+        "")
+    create_command("MobiFlight/B777/BankAngle20",
+        "BankAngle20",
+        "AdjustBankAngle(4)",
+        "",
+        "")
+    create_command("MobiFlight/B777/BankAngle25",
+        "BankAngle25",
+        "AdjustBankAngle(5)",
+        "",
+        "")
 
-        local absDifference = math.abs(difference)
-
-        for i = 1, absDifference do
-            command_once(command)
-        end
+    function Adjust738BankAngle(desiredPosition)
+        AdjustBankAngle(actualPosition, desiredPosition, '1-sim/command/mcpBankAngleSwitch_switch+', '1-sim/command/mcpBankAngleSwitch_switch-')
     end
 end
